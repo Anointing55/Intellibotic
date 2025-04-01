ae44ccf37e3951a4a16b4440b3e68c9c4ead8b24
Anointing55 <anointingomowumi62@gmail.com>
2025-04-01 13:36:37 +0100

    Initial commit with website files

---
+++ scrpt.js
@@ -0,0 +1,5 @@
+// script.js
+function getStarted() {
+    alert("Thank you for getting started with Pate! You'll be redirected to the sign-up page shortly.");
+    window.location.href = "https://your-signup-page-link.com";  // Replace with the actual link for sign-up
+}
